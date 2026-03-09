fee2879e5d616c006437b16fd2682dd936b8c4c7
Hashfi Alfian Ciyuda <hashfi.51.104@gmail.com>
2026-03-09 19:13:25 +0700

    Initial Commit

---
+++ SequisMiniAppTests/SequisMiniAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SequisMiniAppTests.swift
+//  SequisMiniAppTests
+//
+//  Created by Hashfi Alfian Ciyuda on 3/9/26.
+//
+
+import Testing
+@testable import SequisMiniApp
+
+struct SequisMiniAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
